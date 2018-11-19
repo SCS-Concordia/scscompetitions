d236ead31fcac2d9b495ac570b5b3bfcec70aeab
Jacob Gagne <gagne.jacob@gmail.com>
2018-11-19 14:45:54 -0500

    changed title tag

---
+++ docs/build/precache-manifest.eb80bcb22b388d3a5355b6d0daa7adf2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "88e6224101033cc9f318",
+    "url": "/static/js/main.88e62241.chunk.js"
+  },
+  {
+    "revision": "c6513b053c3776c1042f",
+    "url": "/static/js/1.c6513b05.chunk.js"
+  },
+  {
+    "revision": "88e6224101033cc9f318",
+    "url": "/static/css/main.cf558325.chunk.css"
+  },
+  {
+    "revision": "63a1b880e2bd2addd75bc78050838a3a",
+    "url": "/index.html"
+  }
+];
